97129354fc2df56ae080a5884c7f25b3d2ace8c1
Carlos Montano Guevara <carlos.montano@hab.desoft.cu>
2019-05-02 15:36:09 -0400

    Actualizacion

--- app/Http/Controllers/SentenceController.php
+++ app/Http/Controllers/SentenceController.php
@@ -10,7 +10,10 @@ class SentenceController extends Controller
 
     public function countwords(Request $request)
     {
+        $dev = [];
         $sente = $request->sentence;
+        if ($sente == "")
+            return ;
 
             //guardo las palabras en un array
         $array_cadena = str_word_count($sente, 1);
@@ -18,7 +21,7 @@ class SentenceController extends Controller
         $array_cadena = explode(' ', $sente);
 
 
-        $dev = [];
+
         foreach ($array_cadena as $d)
         {
             if (array_key_exists($d, $dev)) {
